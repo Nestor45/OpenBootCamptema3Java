aa4753e3d87409fc3bbd4be15a57f93f7bc8b3ac
nestor45 <nestoralvarez97@gmail.com>
2022-05-26 12:21:04 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String arraynombre [] = {"Nestor","Alvarez","Garcia"};
+
+        for(String nombre: arraynombre) {
+            System.out.print(nombre+" ");
+        }
+    }
+}
